28a17f93f5d9c9ecc2dad40fde86e5afbb0fcfbf
khaoula1972 <khaoula.nakkach@gmail.com>
2023-10-13 01:04:24 +0100

    The base model class
    Co-authored-by: Aziz <ngwk.zz@outlook.com>

--- models/base_model.py
+++ models/base_model.py
@@ -18,9 +18,12 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == '__class__':
-                    continue # To skip the class attribute
+                    continue  # To skip the class attribute
                 elif key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    setattr(
+                            self, key,
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                            )
                 else:
                     setattr(self, key, value)
             if 'id' not in kwargs:
@@ -30,24 +33,31 @@ class BaseModel:
                 # To set creation timestamp
                 self.created_at = datetime.now()
             if 'updated_at' not in kwargs:
-                self.updated_at = datetime.now() # Initial update
+                self.updated_at = datetime.now()  # Initial update
         else:
+            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
-    def __str__(self):
-        """
-        To print
-        """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+            storage.new(self)  # New element
 
     def save(self):
         """
         updates the public instance attribute updated_at
         with the current datetime
         """
+        from models import storage
+
         self.updated_at = datetime.now()
+        storage.save()
+
+    def __str__(self):
+        """
+        To print
+        """
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__
+                )
 
     def to_dict(self):
         """

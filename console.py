8e852de1943f690a6c801e2517008d6bcb849862
khaoula1972 <khaoula.nakkach@gmail.com>
2023-10-13 01:38:12 +0100

    The Console
    Co-authored-by: Aziz <ngwk.zz@outlook.com>

--- console.py
+++ console.py
@@ -45,6 +45,100 @@ class HBNBCommand(cmd.Cmd):
         except ImportError:
             print("** class doesn't exist **")
 
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance
+        based on the class name and id
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+        args = arg.split()
+        if args[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = args[0] + "." + args[1]
+        objects = storage.all()
+        if key in objects:
+            print(objects[key])
+        else:
+            print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """
+        Delete an instance based on class name and id
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+        args = arg.split()
+        if args[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = args[0] + "." + args[1]
+        objects = storage.all()
+        if key in objects:
+            del objects[key]
+            storage.save()
+        else:
+            print("** no instance found **")
+
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances
+        based or not on the class name
+        """
+        args = arg.split()
+        objects = storage.all()
+        everyting = []
+
+        if not arg:
+            for obj in objects.values():
+                everyting.append(str(obj))
+            print(everyting)
+        elif args[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+        else:
+            for key in objects:
+                if key.split(".")[0] == args[0]:
+                    everyting.append(str(objects[key]))
+            print(everyting)
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id
+        by adding or updating attribute
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+        args = arg.split()
+        if args[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = args[0] + "." + args[1]
+        objects = storage.all()
+        if key not in objects:
+            print("** no instance found **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        setattr(objects[key], args[2], args[3])
+        objects[key].save()
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
